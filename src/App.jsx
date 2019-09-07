adcd61593fd2a156233bcc6b5f8202fc24b47a68
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-09-07 14:39:52 +0300

    sc-3

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,16 @@ const Title = styled(Content)`
   color: palevioletred;
 `;
 
+const Card = styled.div`
+  background-color: #d1d1d1;
+  border-radius: 1em;
+  font-weight: 600;
+  padding: 0.5em 0.5em 3em;
+  width: 500px;
+`;
+
 export default () => (
-  <>
+  <Card>
     <Title>
       Styled Title
     </Title>
@@ -23,5 +31,5 @@ export default () => (
       error alias quisquam, perferendis animi
       suscipit dolore sapiente. Nemo, ab odit.
     </Content>
-  </>
+  </Card>
 );
