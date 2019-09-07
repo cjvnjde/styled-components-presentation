5a0fc5876d1c6ed817a14c7fed69d7c10d017248
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-09-07 14:31:17 +0300

    sc-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
 const Content = styled.div`
   font-size: 1.5em;
   text-align: center;
+`;
+
+const Title = styled(Content)`
+  color: palevioletred;
 `;
 
 export default () => (
