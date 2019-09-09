5e93b87bd1e703a3d3f296472e80770383080d5c
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-09-09 21:53:46 +0300

    sc-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
+import Comment from './Comment';
+import CommentForm from './CommentForm';
 
 const Content = styled.div`
   font-size: 1.5em;
@@ -10,18 +13,47 @@ const Title = styled(Content)`
   color: palevioletred;
 `;
 
-export default () => (
-  <>
-    <Title>
-      Styled Title
-    </Title>
-    <Content>
-      Lorem ipsum dolor sit amet,
-      consectetur adipisicing elit.
-      Optio quam, architecto quae dolores
-      nesciunt nihil hic eos voluptas, nemo
-      error alias quisquam, perferendis animi
-      suscipit dolore sapiente. Nemo, ab odit.
-    </Content>
-  </>
-);
+const CommentsList = styled.div`
+  margin: 20px;
+`;
+
+export default () => {
+  const [comments, setComments] = useState([]);
+
+  const commentsList = comments.map(comment => (
+    <Comment
+      name={comment.name}
+      date={comment.date}
+      text={comment.text}
+    />
+  ));
+
+  const addComment = (comment) => {
+    setComments([
+      ...comments,
+      comment,
+    ]);
+  };
+
+  return (
+    <>
+      <Title>
+        Styled Title
+      </Title>
+      <Content>
+        Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit.
+        Optio quam, architecto quae dolores
+        nesciunt nihil hic eos voluptas, nemo
+        error alias quisquam, perferendis animi
+        suscipit dolore sapiente. Nemo, ab odit.
+      </Content>
+      <CommentForm 
+        sendData={addComment}
+      />
+      <CommentsList>
+        {commentsList}
+      </CommentsList>
+    </>
+  );
+}
---
+++ src/CommentForm.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Form = styled.div`
+  width: calc(100% - 40px);
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 30px 20px 10px;
+  margin: 10px 0;
+  background-color: #f2f2f2;
+`;
+
+const TextInput = styled.textarea`
+  width: 100%;
+  height: 60px;
+  resize: none;
+  border-radius: 2px;
+  border: 1px solid #a9a9a9;
+`;
+
+const NameInput = styled.input`
+  width: 100%;
+  height: 20px;
+  resize: none;
+  border-radius: 2px;
+  border: 1px solid #a9a9a9;
+  margin-bottom: 10px;
+`;
+
+const Button = styled.button`
+  background: ${props => props.primary ? "#fdd639" : "#fff"};
+  color: ${props => props.primary ? "#fff" : "#fdd639"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #fdd639;
+  border-radius: 3px;
+
+  :hover {
+    background-color: ${props => props.primary ? "#ffcc00" : "#eee"};
+    color: ${props => props.primary ? "#eee" : "#ffcc00"};
+  }
+
+  :active {
+    background-color: ${props => props.primary ? "#ffde5b" : "#eee"};
+    color: ${props => props.primary ? "#eee" : "#ffde5b"};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const getDate = () => {
+  const months = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря"
+  ];
+
+  const date = new Date();
+
+  return `${date.getDate()} 
+  ${months[date.getMonth()].toLowerCase()} 
+  ${date.getFullYear()}`;
+}
+
+export default ({ sendData }) => {
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+
+  return (
+    <Form>
+      <NameInput
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value)
+        }}
+        placeholder="Your name"
+      />
+      <TextInput
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
+      />
+      <ButtonContainer>
+        <Button onClick={() => {
+          setText('');
+          setName('');
+        }}>
+          Очистить
+        </Button>
+        <Button
+          primary
+          onClick={() => {
+            if (name || text) {
+              sendData({
+                name,
+                text,
+                date: getDate(),
+              });
+            }
+            setText('');
+            setName('');
+          }}
+        >
+          Отправить
+        </Button>
+      </ButtonContainer>
+    </Form>
+  );
+}
